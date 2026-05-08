57efc4b44a6486bb3be45625b85778d2d8f4ba62
SaiSiddartha <peddisaisiddartha1@gmail.com>
2026-05-08 22:26:18 +0400

    Update lipDetection.js

--- js/lipDetection.js
+++ js/lipDetection.js
@@ -3,16 +3,59 @@ function detectWord(
     mouthWidth
 ) {
 
-    if (mouthHeight > 0.06) {
+    const ratio =
+        mouthHeight /
+        mouthWidth;
+
+    // HELLO
+    if (
+        mouthHeight > 0.07
+    ) {
 
         return "HELLO";
     }
 
-    else if (mouthWidth > 0.15) {
+    // YES
+    else if (
+        mouthWidth > 0.18
+    ) {
 
         return "YES";
     }
 
+    // WATER
+    else if (
+        ratio > 0.5
+    ) {
+
+        return "WATER";
+    }
+
+    // HELP
+    else if (
+        mouthHeight > 0.045
+    ) {
+
+        return "HELP";
+    }
+
+    // THANK YOU
+    else if (
+        mouthHeight > 0.03
+    ) {
+
+        return "THANK YOU";
+    }
+
+    // STOP
+    else if (
+        mouthWidth < 0.11
+    ) {
+
+        return "STOP";
+    }
+
+    // NO
     else {
 
         return "NO";
