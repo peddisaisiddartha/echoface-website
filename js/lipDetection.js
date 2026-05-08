36d35efc1fe31badcd17d7d1b9f628b35bf5098b
SaiSiddartha <peddisaisiddartha1@gmail.com>
2026-05-08 22:11:42 +0400

    Add detectWord function for lip detection

--- js/lipDetection.js
+++ js/lipDetection.js
@@ -1,1 +1,20 @@
+function detectWord(
+    mouthHeight,
+    mouthWidth
+) {
 
+    if (mouthHeight > 0.06) {
+
+        return "HELLO";
+    }
+
+    else if (mouthWidth > 0.15) {
+
+        return "YES";
+    }
+
+    else {
+
+        return "NO";
+    }
+}
