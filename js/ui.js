ad04ea9e8e0280b9c670bbbad9443462a8305b8e
SaiSiddartha <peddisaisiddartha1@gmail.com>
2026-05-08 22:09:13 +0400

    Add function to display messages in chat box

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,23 @@
+function addMessage(text, sender) {
 
+    const chatBox =
+    document.getElementById("chatBox");
+
+    const div =
+    document.createElement("div");
+
+    div.classList.add("message");
+
+    div.classList.add(sender);
+
+    div.innerHTML =
+    "<b>" +
+    sender.toUpperCase() +
+    "</b><br>" +
+    text;
+
+    chatBox.appendChild(div);
+
+    chatBox.scrollTop =
+    chatBox.scrollHeight;
+}
