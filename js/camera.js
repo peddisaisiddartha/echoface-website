c9332a64daff89c9cba879b1a3f0ac6245a03fb3
SaiSiddartha <peddisaisiddartha1@gmail.com>
2026-05-08 22:10:17 +0400

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -1,1 +1,14 @@
+const video =
+document.getElementById("video");
 
+async function startCamera() {
+
+    const stream =
+    await navigator.mediaDevices.getUserMedia({
+        video: true
+    });
+
+    video.srcObject = stream;
+
+    await video.play();
+}
