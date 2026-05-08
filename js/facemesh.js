c1db0af0f48e682b5b8ad3120b9977c2f5928c65
SaiSiddartha <peddisaisiddartha1@gmail.com>
2026-05-08 22:10:46 +0400

    Initialize FaceMesh with configuration options

--- js/facemesh.js
+++ js/facemesh.js
@@ -1,1 +1,19 @@
+const faceMesh =
+new FaceMesh({
 
+    locateFile: (file) => {
+
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+    }
+});
+
+faceMesh.setOptions({
+
+    maxNumFaces: 1,
+
+    refineLandmarks: true,
+
+    minDetectionConfidence: 0.5,
+
+    minTrackingConfidence: 0.5
+});
