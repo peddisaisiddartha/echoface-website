c6bcbc0acd73e6993b3c2b3b54f4f2517c90f577
Saisiddartha <liveatlasco@gmail.com>
2026-08-08 22:29:24 +0400

    Fix Supabase environment variable

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -2,12 +2,10 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabasePublishableKey =
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabasePublishableKey) {
-  throw new Error(
-    "Missing Supabase environment variables."
-  );
+  throw new Error("Missing Supabase environment variables.");
 }
 
 export const supabase = createClient(
